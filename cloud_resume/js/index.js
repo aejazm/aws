4bb0e715c4fc43edef0a145ba20290094b9d191d
aejazm <aejaz.muslim@gmail.com>
2024-03-15 15:16:35 -0700

    Added Testfile

--- cloud_resume/js/index.js
+++ cloud_resume/js/index.js
@@ -2,7 +2,7 @@ const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch("https://qt4rtje2snajlu2vbfazwy2mzq0hjlmf.lambda-url.us-west-1.on.aws/");
     let data = await response.json();
-    counter.innerHTML = ` This page has ${data} Views!`;
+    counter.innerHTML = ` This page has ${data} Views !!!`;
 }
 
 updateCounter();
