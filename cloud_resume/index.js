663031f116c32764747c2b3942c2180a31a586b1
Aejaz <Aejaz Muslim>
2024-03-15 10:48:50 -0700

    Initial commit

---
+++ cloud_resume/index.js
@@ -0,0 +1,8 @@
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch("https://qt4rtje2snajlu2vbfazwy2mzq0hjlmf.lambda-url.us-west-1.on.aws/");
+    let data = await response.json();
+    counter.innerHTML = ` This page has ${data} Views!`;
+}
+
+updateCounter();
